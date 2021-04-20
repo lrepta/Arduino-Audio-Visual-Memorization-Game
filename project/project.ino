8aff4ac23a0c7f731f7750470e9a5d83b352b5d0
lrepta2 <lrepta2@uic.edu>
2021-04-20 05:14:23 -0500

    Did some work on the record input function, added some planning comments, and a little restructuring of the loop function for code clarity

--- project/project.ino
+++ project/project.ino
@@ -27,6 +27,9 @@ const int soundPin = 9;
 const int softwareRXPin = 11;
 const int softwareTXPin = 12;
 
+// Could secretely decrement these values as the user advances levels, making each
+// stimulus shorter and as such harder to remember
+// Possible adjuster if we find even the highest level is too easy
 const int shortLength = 500; // Length of the short flashes/buzzes in milliseconds
 const int longLength = 1000; // Length of the long flashes/buzzes in milliseconds
 
@@ -60,16 +63,40 @@ void setup() {
 // someDataStructure correctSequence;
 // someDataStructure userSequence;
 
+// user sequence is of max length 15, initialized values to 0 as 1-4 is used for LED/Sound
+int userSequence[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int userSequenceLength = 0;
+
 int recordInput() {
     // record input on the buttons until the user enters an incorrect input?
     // Otherwise we will need another button or something for the user to indicate when
     // they are done entering what they think the sequence is
-    while() {
+    int count = 0;
+    while(count < 15) {
         // record user input, with proper debouncing etc
+
+        // Check if user pressed short LED button
+            // userSequence[count++] = 1;
+        // Check if user pressed long LED button
+            // userSequence[count++] = 2;
+        
+        // Check if user pressed short sound button
+            // userSequence[count++] = 3;
+        // Check if user pressed long sound button
+            // userSequence[count++] = 4;
+
+        // If we decide to go that route, check if  
+        // user pressed submit sequence button
+
         if (input sequence is still correct) {
             continue;
         } else {
             // Incorrect input
+
+            //reset the userSequence
+            for (int i = 0; i < 15; i++) {
+                userSequence[i] = 0;
+            }
             return 0;
         }
     }
@@ -178,7 +205,7 @@ void displaySequence() {
                 break;
             default:
                 // This case should never happen, only happens if you read the
-                // uninitalized 0's -> somethng is wrong!
+                // uninitalized 0's -> something is wrong!
                 break;
         }
     }
@@ -186,14 +213,22 @@ void displaySequence() {
 
 void loop() {
     // Lots of repeated code, could be made prettier
+    generateSequence();
+
   if (level == 1) {
+      // This is where the blocking call for recording user input occurs
+      // Either returns 1 if the user got the sequence right, or returns
+      // 0 if they get it wrong
     if (recordInput() == 1) {
         level++;
-        generateSequence();
-    } else {
+    } 
+    // Moved generate sequence out of if and else to make code clearer
+    // Don't think the else is even necessary unless we want something else
+    // to happen when the user fails to guess a level at level 1
+    else {
         // reset the level with a new sequence
-        generateSequence();
     }
+    generateSequence();
   }
   else if (level == 2) {
       if (recordInput() == 1) {
